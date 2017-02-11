3179b18f22c34141a79b61a345bcba1983b1e01c
Zhiyu Max Mao <zhuymao@gmail.com>
2017-02-11 15:57:24 -0800

    update some node module

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 /**
  * Server Deployment
  * --------------
- * Created by: Lifan Chen
- * Last updated: Feb 2
+ * Created by: Lifan Chen, Max Mao, Xiaowen Mao
+ * Last updated: Feb 10
  */
 var PORT = 3000;
 
@@ -10,18 +10,29 @@ var PORT = 3000;
 // that makes nontrivial applications easier to build
 var express = require('express');
 
+// dependencies
+var http = require('http');
+var path = require('path');
+var handlebars = require('express3-handlebars');
+var index = require('./routes/index');
 // Create the server instance
 var app = express();
 
 // Print logs to the console and compress pages we send
+app.set('views', path.join(__dirname, 'views'));
 app.use(express.logger());
 app.use(express.compress());
+app.engine('handlebars',handlebars());
+app.set('view engine', 'handlebars');
 
 // Return all pages in the /static directory
 // whenever they are requested at '/'
 // e.g., http://localhost:3000/index.html
 // maps to /static/index.html on this machine
 app.use(express.static(__dirname + '/static'));
+
+// add routes here
+app.get('/',index.view);
 
 // Start the server
 var port = process.env.PORT || PORT; // 80 for web, 3000 for development
