338442aab1a90387c2eff428376fa2905eea7807
Lifan Chen <lic124@ucsd.edu>
2017-02-17 02:13:10 -0800

    add fake user

--- static/js/room.js
+++ static/js/room.js
@@ -1,5 +1,26 @@
 'use strict';
 
+var i = 0;
+
+var Fake = [
+  'Hi there, I\'m Trump and you?',
+  'Nice to meet you',
+  'How are you?',
+  'Not too bad, thanks',
+  'What do you do?',
+  'That\'s awesome',
+  'Codepen is a nice place to stay',
+  'I think you\'re a nice person',
+  'Why do you think that?',
+  'Can you explain?',
+  'Anyway I\'ve gotta go now',
+  'It was a pleasure chat with you',
+  'Time to make a new codepen',
+  'Bye',
+  ':)'
+];
+
+
 $(document).ready(function() {
 	initializePage();
 })
@@ -23,8 +44,29 @@ function sendMessage(e) {
       "content":content
     };
     $.post('/room/message' + roomName, message, renderMessage);
-  }
+
+	}
 }
+
+
+function fakeMessage(){
+	console.log("faked");
+	var content = Fake[i];
+	i ++;
+  var date = new Date();
+  var time = date.getHours() + ":" + date.getMinutes();
+  var addHTML = '<div class="message-inverse">\
+    <div class="msg-content">' + content +
+    '</div>\
+    <img id="" class="" src="./img/trump.jpeg" alt="">\
+    <div class="timestamp" id="">' + time + '\
+    </div>\
+  </div>';
+  $('.messages').append(addHTML);
+  updateScrollBar();
+}
+
+
 
 function renderMessage(data){
   var content = data.content;
@@ -39,6 +81,9 @@ function renderMessage(data){
   </div>';
   $('.messages').append(addHTML);
   updateScrollBar();
+	setTimeout(function() {
+	fakeMessage();
+	}, 1000 + (Math.random() * 20) * 100);
 }
 
 function updateScrollBar() {
