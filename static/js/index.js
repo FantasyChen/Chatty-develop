eb114edf0292aefcc4a33e9e19e6d13d40c9c7c1
Lifan Chen <lic124@ucsd.edu>
2017-02-17 01:20:15 -0800

    fix stars/ add forget name

---
+++ static/js/index.js
@@ -0,0 +1,21 @@
+'use strict';
+
+$(document).ready(function() {
+	initializePage();
+})
+
+function initializePage() {
+  $('.collect').click(changeCollect);
+}
+
+
+function changeCollect(e){
+  e.preventDefault();
+  var origImg = $(this).find('img').attr("src");
+  if(origImg == "./img/star_gold_small.png"){
+    origImg = $(this).find('img').attr("src","./img/star_grey_small.png" );
+  }
+  else{
+    origImg = $(this).find('img').attr("src","./img/star_gold_small.png" );
+  }
+}
