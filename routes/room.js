20684d0b58a6a261a65e7374db5fdb97c3d98124
FantasyChen <524413916@qq.com>
2017-02-20 22:13:49 -0800

    change title

--- routes/room.js
+++ routes/room.js
@@ -9,27 +9,3 @@ exports.view = function(req, res){
     'name' : programName
   });
 };
-
-
-// Chatroom sockets is handled here
-exports.socketListener = function(socket){
-  var room = "";
-  socket.on("sendMsg", function(data){
-    console.log("Server received message: " + data.content);
-    socket.broadcast.to(room).emit('receiveMsg', data);   // broadcast the message to everyone in the room except self.
-  });
-  socket.on('login', function(data){
-    console.log("An user have logged in");
-    room = data.programName;
-    socket.join(room, function(){
-      if(userCount[room] == undefined){
-        userCount[room] = 1;
-      }
-      else{
-        userCount[room] += 1;
-      }
-      socket.to(room).emit("userCount", userCount[room]);
-    });
-  });
-
-}
