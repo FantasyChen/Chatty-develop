f19d3f880174b71498a0c74564cbb01a65b18179
FantasyChen <524413916@qq.com>
2017-02-25 21:59:35 -0800

    fix static login.js

--- static/js/login.js
+++ static/js/login.js
@@ -7,14 +7,5 @@ $(document).ready(function() {
 })
 
 function initializePage() {
-  $('.registerBtn').click(showRegister);
-}
 
-// function showRegister(e) {
-//   e.preventDefault();
-// 	console.log("JS connected");
-//   var page = $('.login-box');
-//   var $confirmPwd = $("<div class='confirmPwd-box'><input type='text' class='confirmPwd' placeholder='Confirm Your Password'/></div>");
-//   $("document").removeClass(".loginBtn");
-//   page.append($confirmPwd);
-// }
+}
