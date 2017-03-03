db0ab459bafc979200ed5a1ee2c190cd008f0d66
FantasyChen <524413916@qq.com>
2017-03-02 16:28:11 -0800

    fix removing star in favorite page

--- static/js/favorites.js
+++ static/js/favorites.js
@@ -1,7 +1,15 @@
 'use strict';
 
+var isAuthen = false;
+var user;
+
 $(document).ready(function() {
 	initializePage();
+	var userString = $('#savedLoginInfo').text().toString();
+	if(userString.length>2){
+		isAuthen = true;
+		user = JSON.parse(userString);
+		}
 })
 
 function initializePage() {
@@ -12,10 +20,21 @@ function initializePage() {
 function changeCollect(e){
   e.preventDefault();
   var origImg = $(this).find('img').attr("src");
+	var programID = $(this).attr('id');
   if(origImg == "./img/star_gold_small.png"){
+		$.get('/addFavorite',{
+			"userID": user.userID,
+			"program": programID,
+			"type": "remove"
+		});
     origImg = $(this).find('img').attr("src","./img/star_grey_small.png" );
   }
   else{
+		$.get('/addFavorite', {
+			"userID": user.userID,
+			"program": programID,
+			"type": "add"
+		});
     origImg = $(this).find('img').attr("src","./img/star_gold_small.png" );
   }
 }
